859a3df1210443917f44f445545e7e64c0dc26b7
Marvin <marvin.volkmann.mv@gmail.com>
2022-04-24 14:35:56 +0200

    implemented soft 404-Error

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import PageHome from '@/components/PageHome'
 import PageThreadShow from '@/components/PageThreadShow'
+import PageNotFound from '@/components/PageNotFound'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
@@ -14,6 +15,11 @@ const routes = [
         component: PageThreadShow,
         props: true
     },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: PageNotFound
+    }
 ]
 
 export default createRouter({
